46c03263a5a596e8542a6666e682d9eee046da59
Alexandr <a.sh.10031987@gmail.com>
2018-04-08 19:32:17 +0300

    change isAnagram

--- src/harman/tasks/strings/Task4.java
+++ src/harman/tasks/strings/Task4.java
@@ -1,27 +1,15 @@
 package harman.tasks.strings;
 
-import java.util.HashMap;
-
 public class Task4 {
     public boolean isAnagram(String first, String second) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        char[] firstStr = first.replace(" ", "").toLowerCase().toCharArray();
-        char[] secondStr = second.replace(" ", "").toLowerCase().toCharArray();
+        String firstStr = first.replace(" ", "").toLowerCase();
+        String secondStr = second.replace(" ", "").toLowerCase();
 
-        for (char ch : firstStr) {
-            map.merge(ch, 1, (a, b) -> a + b);
-        }
-
-        for (char ch : secondStr) {
-            if (map.get(ch) == null) {
+        for (int i = 1; i < firstStr.length(); i++) {
+            if (!firstStr.contains(secondStr.substring(i, i + 1))) {
                 return false;
             }
-            if (map.get(ch) - 1 == 0) {
-                map.remove(ch);
-            } else {
-                map.put(ch, map.get(ch) - 1);
-            }
         }
-        return map.isEmpty();
+        return true;
     }
 }
