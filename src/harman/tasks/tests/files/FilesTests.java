4cd9519f32618fcd8d00e16aa6936d3fe3926b06
Alexandr <a.sh.10031987@gmail.com>
2018-04-08 19:48:47 +0300

    chande FilesTests

--- src/harman/tasks/tests/files/FilesTests.java
+++ src/harman/tasks/tests/files/FilesTests.java
@@ -8,39 +8,32 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class FilesTests {
-    private final String DONE = "Test done";
-    private final String FAILED = "Test failed";
+    public void testStatus(String taskName, boolean test) {
+        String status = test ? "Test done" : "Test failed";
+        System.out.printf("%s: %s\n", taskName, status);
+    }
 
-    public String testTask1() {
+    public boolean testTask1() {
         List<String> resultList = new LinkedList<>();
         Task1 task1 = new Task1(resultList);
         String file = "D:\\Programming\\Harman\\Tasks\\src\\harman\\tasks\\files\\text.txt";
         task1.printReversList(file);
         List<String> exceptList = new LinkedList<>(Arrays.asList("line - 3", "line - 2", "line - 1"));
-        return exceptList.equals(resultList) ? DONE : FAILED;
+        return exceptList.equals(resultList);
     }
 
-    public String testTask2() {
+    public boolean testTask2() {
         List<String> resultList = new LinkedList<>();
         Task2 task2 = new Task2(resultList);
         task2.printListOfFiles("D:\\textFiles");
         List<String> exceptList = new LinkedList<>(Arrays.asList("text1.txt", "text2.txt", "text3.txt"));
-        return exceptList.equals(resultList) ? DONE : FAILED;
+        return exceptList.equals(resultList);
     }
-
-
-
-
-
-
 
     public static void main(String[] args) {
         FilesTests filesTests = new FilesTests();
 
-        System.out.println("Task1: " + filesTests.testTask1());
-        System.out.println("Task2: " + filesTests.testTask2());
-//        System.out.println("Task3: " + filesTests.testTask3());
-//        System.out.println("Task4: " + filesTests.testTask4());
-//        System.out.println("Task5: " + filesTests.testTask5());
+        filesTests.testStatus("Task1", filesTests.testTask1());
+        filesTests.testStatus("Task2", filesTests.testTask2());
     }
 }
